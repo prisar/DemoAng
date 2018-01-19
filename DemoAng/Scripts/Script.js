8c706e7ab5c5e333b7ad7cd423bb8236c930025b
Pritimay Sarkar <pritimaysarkar2010@gmail.com>
2018-01-19 20:48:34 +0530

    Add project files.

---
+++ DemoAng/Scripts/Script.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="angular.min.js" />
+
+//var myApp = angular.module("myModule", []);
+
+//var myController = function ($scope) {
+//    $scope.message = "Angular";
+//};
+
+//myApp.controller("myController", myController)
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function ($scope) {
+    $scope.message = "Angular";
+});
