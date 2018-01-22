bfcc9f737a5d9ff14dc3cd4b044cac100f39b30d
Pritimay Sarkar <pritimaysarkar2010@gmail.com>
2018-01-22 06:26:15 +0530

    ng-model directive

--- DemoAng/Scripts/Script.js
+++ DemoAng/Scripts/Script.js
@@ -3,11 +3,5 @@
 var myApp = angular
     .module("myModule", [])
     .controller("myController", function ($scope) {
-        var country = {
-            name: "USA",
-            capital: "Washington, D.C.",
-            flag: "/Images/usa-flag.png"
-        };
-
-        $scope.country = country;
+        $scope.message = "Hello Angular!";
     });
