6024ce5026ed75a9bfbf67c65d021de694ccb926
Pritimay Sarkar <pritimaysarkar2010@gmail.com>
2018-01-22 06:38:01 +0530

    ng-model use with employee object

--- DemoAng/Scripts/Script.js
+++ DemoAng/Scripts/Script.js
@@ -3,5 +3,11 @@
 var myApp = angular
     .module("myModule", [])
     .controller("myController", function ($scope) {
-        $scope.message = "Hello Angular!";
+        var employee = {
+            firstName: "Ben",
+            lastName: "Hastings",
+            gender: "Male"
+        };
+
+        $scope.employee = employee;
     });
